b7ae580e805137cb757fecc070dec11ffa89f9c8
Mike <mpghemingway@gmail.com>
2021-06-01 12:00:00 -0400

    Check for ref

--- src/ui/Speed.js
+++ src/ui/Speed.js
@@ -4,7 +4,7 @@ import { useStore } from '../utils/store'
 export function Speed() {
   const ref = useRef()
   useEffect(() => {
-    const interval = setInterval(() => (ref.current.innerText = `${useStore.getState().speed.toFixed()} mph`), 60)
+    const interval = setInterval(() => (ref.current ? ref.current.innerText = `${useStore.getState().speed.toFixed()} mph`: null), 60)
     return () => clearInterval(interval)
   }, [])
   return <div ref={ref} className="speed" />
