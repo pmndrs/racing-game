7aa5a925ac3ff7e6b794cd94f4261be69b601bcb
Michael Hemingway <mpghemingway@gmail.com>
2021-06-01 21:06:13 -0400

    Remove visible Geometry generation method

--- src/utils/terrain.js
+++ src/utils/terrain.js
@@ -1,7 +1,5 @@
-import * as THREE from 'three'
 import { useTexture } from '@react-three/drei'
-import { useRef, useEffect, Suspense, useMemo } from 'react'
-
+import { useMemo } from 'react'
 import { useHeightfield } from '@react-three/cannon'
 
 // returns matrix data to be passed to heightfield
@@ -14,7 +12,7 @@ function createHeightfieldMatrix(image) {
   const h = image.height
   const canvas = document.createElement('canvas')
   const ctx = canvas.getContext('2d')
-  const scale = 40
+  const scale = 20
   let p, row
 
   canvas.width = w
@@ -33,37 +31,6 @@ function createHeightfieldMatrix(image) {
   return matrix
 }
 
-function HeightmapGeometry({ heights, elementSize, ...rest }) {
-  const ref = useRef()
-
-  useEffect(() => {
-    const dx = elementSize
-    const dy = elementSize
-
-    /* Create the vertex data from the heights. */
-    const vertices = heights.flatMap((row, i) => row.flatMap((z, j) => [i * dx, j * dy, z]))
-
-    /* Create the faces. */
-    const indices = []
-    for (let i = 0; i < heights.length - 1; i++) {
-      for (let j = 0; j < heights[i].length - 1; j++) {
-        const stride = heights[i].length
-        const index = i * stride + j
-        indices.push(index + 1, index + stride, index + stride + 1)
-        indices.push(index + stride, index + 1, index)
-      }
-    }
-
-    ref.current.setIndex(indices)
-    ref.current.setAttribute('position', new THREE.Float32BufferAttribute(vertices, 3))
-    ref.current.computeVertexNormals()
-    ref.current.computeBoundingBox()
-    ref.current.computeBoundingSphere()
-  }, [heights])
-
-  return <bufferGeometry {...rest} ref={ref} />
-}
-
 export function Heightfield(props) {
   const { elementSize, position, rotation, ...rest } = props
   const heightmap = useTexture('/heightmap_512.png')
@@ -80,10 +47,5 @@ export function Heightfield(props) {
     rotation
   }))
 
-  return (
-    <mesh ref={ref} castShadow receiveShadow {...rest}>
-      <meshNormalMaterial flatShading/>
-      <HeightmapGeometry heights={heights} elementSize={elementSize} />
-    </mesh>
-  )
+  return null
 }
