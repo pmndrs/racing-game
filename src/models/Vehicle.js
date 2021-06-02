0af893dd1a85778a40b598e12ff7e898bb8eed5a
Lewy Blue <lewy@discoverthreejs.com>
2021-06-02 14:53:17 +0700

    improved follow cam

--- src/models/Vehicle.js
+++ src/models/Vehicle.js
@@ -7,9 +7,14 @@ import { Chassis } from './Chassis'
 import { Wheel } from './Wheel'
 import { useStore } from '../utils/store'
 import { Dust } from '../effects/Dust'
-import { vehicleStart } from '../constants'
 
 const v = new THREE.Vector3()
+const cameraPositionTarget = new THREE.Vector3()
+const cameraLookAtPosition = new THREE.Vector3()
+const cameraLookAtTarget = new THREE.Vector3()
+const cameraTarget = new THREE.Object3D()
+const cameraDistance = -5
+const cameraDamping = 5
 
 export function Vehicle(props) {
   const camera = useRef()
@@ -20,8 +25,12 @@ export function Vehicle(props) {
   const [vehicle, api] = useRaycastVehicle(() => raycast)
 
   useLayoutEffect(() => {
-    // Look at is causing the weird spin in the beginning
-    camera.current.lookAt(raycast.chassisBody.current.position)
+    raycast.chassisBody.current.getWorldPosition(cameraLookAtPosition)
+    raycast.chassisBody.current.getWorldPosition(cameraLookAtTarget)
+    cameraLookAtPosition.copy(raycast.chassisBody.current.position)
+    raycast.chassisBody.current.add(cameraTarget)
+    cameraTarget.position.set(0, 2, cameraDistance)
+
     // Subscriptions
     const vSub = raycast.chassisBody.current.api.velocity.subscribe((velocity) => set({ velocity, speed: v.set(...velocity).length() }))
     const sSub = api.sliding.subscribe((sliding) => set({ sliding }))
@@ -42,19 +51,20 @@ export function Vehicle(props) {
     for (let s = 0; s < 2; s++) api.setSteeringValue(steeringValue, s)
     for (let b = 2; b < 4; b++) api.setBrake(brake ? (forward ? maxBrake / 1.5 : maxBrake) : 0, b)
     if (reset) {
-      raycast.chassisBody.current.api.position.set(vehicleStart.position[0], vehicleStart.position[1], vehicleStart.position[2])
+      raycast.chassisBody.current.api.position.set(0, 0.5, 0)
       raycast.chassisBody.current.api.velocity.set(0, 0, 0)
-      raycast.chassisBody.current.api.angularVelocity.set(vehicleStart.angularVelocity[0], vehicleStart.angularVelocity[1], vehicleStart.angularVelocity[2])
-      raycast.chassisBody.current.api.rotation.set(vehicleStart.rotation[0], vehicleStart.rotation[1], vehicleStart.rotation[2])
+      raycast.chassisBody.current.api.angularVelocity.set(0, 0.5, 0)
+      raycast.chassisBody.current.api.rotation.set(0, -Math.PI / 4, 0)
     }
 
-    // left-right, up-down, near-far
-    camera.current.position.lerp(
-      v.set((Math.sin(steeringValue) * speed) / 2.5, 1.25 + (engineValue / 1000) * -0.5, -5 - speed / 15 + (brake ? 1 : 0)),
-      delta,
-    )
-    // left-right swivel
-    camera.current.rotation.z = THREE.MathUtils.lerp(camera.current.rotation.z, Math.PI + (-steeringValue * speed) / 45, delta)
+    raycast.chassisBody.current.getWorldPosition(cameraLookAtPosition)
+    cameraLookAtTarget.lerp(cameraLookAtPosition, delta * cameraDamping)
+    camera.current.lookAt(cameraLookAtTarget)
+
+    cameraTarget.position.z = cameraDistance - speed / 50
+    cameraTarget.getWorldPosition(cameraPositionTarget)
+    camera.current.position.lerp(cameraPositionTarget, delta * cameraDamping)
+
     // lean chassis
     raycast.chassisBody.current.children[0].rotation.z = THREE.MathUtils.lerp(
       raycast.chassisBody.current.children[0].rotation.z,
@@ -77,9 +87,9 @@ export function Vehicle(props) {
         shadow-camera-top={150}
         shadow-camera-bottom={-150}
       />
+      <PerspectiveCamera ref={camera} makeDefault fov={75} rotation={[0, Math.PI, 0]} position={[0, 10, -20]} />
       <group ref={vehicle} position={[0, -0.4, 0]}>
         <Chassis ref={raycast.chassisBody} rotation={props.rotation} position={props.position} angularVelocity={props.angularVelocity}>
-          <PerspectiveCamera ref={camera} makeDefault fov={75} rotation={[0, Math.PI, 0]} position={[0, 10, -20]} />
           {light && <primitive object={light.target} />}
         </Chassis>
         <Wheel ref={raycast.wheels[0]} radius={config.radius} leftSide />
