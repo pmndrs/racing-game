7e17987745500b1291421278dc3cc0d2fd8e218c
Paul Henschel <drcmda@gmail.com>
2021-06-02 19:38:50 +0200

    make gauge smaller

--- src/ui/Speed.js
+++ src/ui/Speed.js
@@ -9,7 +9,7 @@ export function Speed() {
   useEffect(() => {
     if (textRef.current !== null) {
       const interval = setInterval(() => {
-        const { speed, config } = useStore.getState();
+        const { speed, config } = useStore.getState()
         const computedSpeed = speed * 1.5
         textRef.current.innerText = computedSpeed.toFixed()
         gaugeRef.current.setAttribute('offset', Math.max(1 - computedSpeed / config.maxSpeed, 0))
@@ -19,10 +19,12 @@ export function Speed() {
   }, [])
   return (
     <div className="speed">
-      <span ref={textRef} /> mph
       <div className="speed-gauge">
         <SpeedmeterBackground className="speed-background" gaugeRef={gaugeRef} />
         <SpeedmeterForeground className="speed-foreground" />
+      </div>
+      <div className="speed-text">
+        <span ref={textRef} /> mph
       </div>
     </div>
   )
