21abc648d18d26db89e6cb75cca25fcc707770e8
njm222 <nathan.john.menezes@gmail.com>
2021-06-03 13:04:37 +0530

    stuck on skid rotation

--- src/effects/Skid.js
+++ src/effects/Skid.js
@@ -8,8 +8,10 @@ const m = new THREE.Matrix4()
 const o = new THREE.Object3D()
 const q = new THREE.Quaternion()
 
+const skidLength = 500
+
 export function Skid({ opacity = 0.8 }) {
-  const { wheels } = useStore((state) => state.raycast)
+  const { chassisBody, wheels } = useStore((state) => state.raycast)
   const skid = useRef()
 
   let index = 0
@@ -17,28 +19,31 @@ export function Skid({ opacity = 0.8 }) {
   let intensity = 0
 
   function setItemAt(target, obj, i, intensity) {
-    o.position.set(obj.position.x, obj.position.y - 0.5, obj.position.z)
+    const tempObj = new THREE.Object3D()
+    tempObj.position.set(obj.position.x, obj.position.y - 0.4, obj.position.z)
+    tempObj.rotation.set(0, 0, 0) // need to match the rotation of the wheels
     const scale = Math.random() * intensity
-    o.scale.set(scale, scale * 1.25, scale)
-    o.updateMatrix()
-    target.setMatrixAt(i, o.matrix)
+    tempObj.scale.set(scale, scale, scale)
+    tempObj.updateMatrix()
+    target.setMatrixAt(i, tempObj.matrix)
     skid.current.instanceMatrix.needsUpdate = true
   }
 
   useFrame((state, delta) => {
     const store = useStore.getState()
     const { controls, sliding, speed } = useStore.getState()
+
     intensity = THREE.MathUtils.lerp(intensity, (((sliding || controls.brake) * speed) / 40), delta * 8)
 
     if (state.clock.getElapsedTime() - time > 0.02) {
       time = state.clock.getElapsedTime()
-      // Set new skid
+      // Set new skids
       setItemAt(skid.current, wheels[2].current, index++, intensity)
       setItemAt(skid.current, wheels[3].current, index++, intensity)
       if (index === 200) index = 0
     } else {
       // Shrink old one
-      for (let i = 0; i < 200; i++) {
+      for (let i = 0; i < skidLength; i++) {
         skid.current.getMatrixAt(i, m)
         m.decompose(o.position, q, v)
         o.scale.set(Math.max(0, v.x - 0.001), Math.max(0, v.y - 0.001), Math.max(0, v.z - 0.001))
@@ -50,8 +55,8 @@ export function Skid({ opacity = 0.8 }) {
   })
 
   return (
-    <instancedMesh ref={skid} args={[null, null, 200]}>
-      <boxGeometry args={[2, 1, 2]} />
+    <instancedMesh ref={skid} args={[null, null, skidLength]}>
+      <boxGeometry args={[3, 0.1, 1]} />
       <meshBasicMaterial color="black" transparent opacity={opacity} depthWrite={false} />
     </instancedMesh>
   )
