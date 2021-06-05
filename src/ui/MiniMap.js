1f7eb4b5a4e568352264d22939b3b9a755cd3852
Denis Zimmermann <denis.zimmermann@comwatt.com>
2021-06-05 15:02:28 +0200

    feat: display position of player on the minimap

--- src/ui/MiniMap.js
+++ src/ui/MiniMap.js
@@ -1,27 +1,36 @@
 import { OrthographicCamera, useFBO } from '@react-three/drei'
 import { createPortal, useFrame, useThree } from '@react-three/fiber'
-import { useEffect, useLayoutEffect, useMemo, useRef } from 'react'
+import { useEffect, useLayoutEffect, useMemo, useRef, useState } from 'react'
 import { Box3, Matrix4, Scene, Vector3 } from 'three'
 import { levelLayer } from '../enums'
 import { useStore } from '../store'
 
-const levelCenter = new Vector3()
-function useLevelDimensions(callback) {
-  const callbackRef = useRef(callback)
+function useLevelDimensions() {
+  const [levelBox] = useState(new Box3())
+  const [levelCenter] = useState(new Vector3())
+  const [levelDimensions] = useState(new Vector3())
   const level = useStore((state) => state.level)
 
   useEffect(() => {
-    const levelBox = new Box3().setFromObject(level.current)
+    const box = new Box3().setFromObject(level.current)
+    levelBox.copy(box)
     levelBox.getCenter(levelCenter)
-    callbackRef.current({ levelBox, levelCenter })
+    levelBox.getSize(levelDimensions)
   }, [])
+
+  return {
+    levelBox,
+    levelCenter,
+    levelDimensions,
+  }
 }
 
 function MiniMapTexture({ buffer }) {
   const camera = useRef()
   const { gl, scene } = useThree(({ gl, scene }) => ({ gl, scene }))
+  const { levelBox, levelCenter } = useLevelDimensions()
 
-  useLevelDimensions(({ levelBox, levelCenter }) => {
+  useEffect(() => {
     gl.setRenderTarget(buffer)
     camera.current.bottom = levelBox.min.z - levelCenter.z
     camera.current.top = levelBox.max.z - levelCenter.z
@@ -31,7 +40,7 @@ function MiniMapTexture({ buffer }) {
     camera.current.updateProjectionMatrix()
     gl.render(scene, camera.current)
     gl.setRenderTarget(null)
-  })
+  }, [levelBox, levelCenter])
 
   useLayoutEffect(() => {
     camera.current.layers.disableAll()
@@ -41,22 +50,35 @@ function MiniMapTexture({ buffer }) {
   return <OrthographicCamera ref={camera} makeDefault={false} rotation={[-Math.PI / 2, 0, 0]} near={20} far={500} />
 }
 
-function MiniMap() {
+function MiniMap({ size = 300 }) {
   const virtualScene = useMemo(() => new Scene(), [])
   const buffer = useFBO(600, 600)
   const miniMapCamera = useRef()
   const miniMap = useRef()
-
+  const { gl, camera, scene, size: screenSize } = useThree(({ camera, gl, scene, size }) => ({ gl, camera, scene, size }))
+  const [screenPosition, setScreenPosition] = useState(new Vector3(screenSize.width / 2 - size / 2, screenSize.height / 2 - size / 2, 0))
+  const player = useRef()
   const matrix = new Matrix4()
-  const { gl, camera, scene, size } = useThree(({ camera, gl, scene, size }) => ({ gl, camera, scene, size }))
+  const { levelCenter, levelDimensions } = useLevelDimensions()
+  const chassisBody = useStore((state) => state.raycast.chassisBody)
+  const direction = new Vector3()
+
+  useEffect(() => {
+    setScreenPosition(new Vector3(screenSize.width / 2 - size / 2, screenSize.height / 2 - size / 2, 0))
+  }, [screenSize])
 
   useFrame(() => {
     matrix.copy(camera.matrix).invert()
     miniMap.current.quaternion.setFromRotationMatrix(matrix)
+    player.current.quaternion.setFromRotationMatrix(matrix)
     gl.autoClear = true
     gl.render(scene, camera)
     gl.autoClear = false
     gl.clearDepth()
+    direction.subVectors(chassisBody.current.position, levelCenter)
+    const ratioX = size / levelDimensions.x
+    const ratioY = levelDimensions.y / size
+    player.current.position.set(screenPosition.x + direction.x * ratioX, screenPosition.y - direction.z * ratioY, 0)
     gl.render(virtualScene, miniMapCamera.current)
   }, 1)
 
@@ -65,9 +87,10 @@ function MiniMap() {
       {createPortal(
         <>
           <OrthographicCamera ref={miniMapCamera} makeDefault={false} position={[0, 0, 100]} />
-          <sprite ref={miniMap} position={[size.width / 2 - 200, size.height / 2 - 200, 0]} scale={[300, 300, 1]}>
+          <sprite ref={miniMap} position={screenPosition} scale={[size, size, 1]}>
             <spriteMaterial map={buffer.texture} />
           </sprite>
+          <sprite ref={player} position={screenPosition} scale={[size / 30, size / 30, 1]} />
         </>,
         virtualScene,
       )}
