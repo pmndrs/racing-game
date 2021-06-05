b0a05f9f0eec2522cd52552736b207dd15ca41b1
Jonathan Chai <chaijw@usc.edu>
2021-06-05 00:48:45 -0700

    made loader to fallback

--- src/ui/Overlay.js
+++ src/ui/Overlay.js
@@ -1,45 +1,53 @@
 import { Suspense, useEffect, useState } from 'react'
 import { Footer } from '@pmndrs/branding'
+import { useProgress, Html } from '@react-three/drei'
 import { Keys } from './Help'
 import { useStore } from '../store'
 
-import { Loader } from './Loader';
-
 function Ready({ setReady }) {
-    useEffect(() => () => void setReady(true), [])
-    return null;
+  const { progress } = useProgress();
+  return (
+    <>
+      <div
+        className="fullscreen"
+        style={{
+          backgroundImage: 'url(/graphics/loadingscreen.png)',
+          backgroundSize: 'cover',
+        }}>
+        <div className="loading-title">
+          <h1>POIMANDRES RACING GAME</h1>
+          <h2>{progress}%</h2>
+        </div>
+      </div>
+    </>
+  )
 }
 
 export function Overlay({ children }) {
-    const [ready, setReady] = useState(false)
-    const [clicked, setClicked] = useState(false)
-    const set = useStore((state) => state.set)
+  const [clicked, setClicked] = useState(false)
+  const set = useStore((state) => state.set)
 
-    useEffect(() => {
-        if (clicked && ready) set({ ready: true })
-    }, [ready, clicked])
+  useEffect(() => {
+    if (clicked) set({ ready: true })
+  }, [clicked])
 
-    return (
-        <>
-            <Suspense fallback={<Ready setReady={setReady} />}>{children}</Suspense>
-            <div className={`fullscreen bg ${ready ? 'ready' : 'notready'} ${clicked && 'clicked'}`}>
-                {!ready ? <Loader /> :
-                    <>
-                        <div className="stack">
-                            <Keys style={{ paddingBottom: 20 }} />
-                            <a href="#" onClick={() => ready && setClicked(true)}>
-                                {!ready ? 'loading' : 'Click to continue'}
-                            </a>
-                        </div>
-                        <Footer
-                            date="2. June"
-                            year="2021"
-                            link1={<a href="https://github.com/pmndrs/react-three-fiber">@react-three/fiber</a>}
-                            link2={<a href="https://github.com/pmndrs/racing-game">/racing-game</a>}
-                        />
-                    </>
-                }
-            </div>
-        </>
-    )
+  return (
+    <>
+      <Suspense fallback={<Ready />}>{children}</Suspense>
+      <div className={`fullscreen bg ready ${clicked && 'clicked'}`}>
+        <div className="stack">
+          <Keys style={{ paddingBottom: 20 }} />
+          <a href="#" onClick={() => setClicked(true)}>
+            Click to continue
+          </a>
+        </div>
+        <Footer
+          date="2. June"
+          year="2021"
+          link1={<a href="https://github.com/pmndrs/react-three-fiber">@react-three/fiber</a>}
+          link2={<a href="https://github.com/pmndrs/racing-game">/racing-game</a>}
+        />
+      </div>
+    </>
+  )
 }
