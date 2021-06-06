80934122c737b9cdfbfd336d647ec4d69cb15400
Gusted <williamzijl7@hotmail.com>
2021-06-06 13:47:20 +0200

    pretty-fy

--- src/controls/Inactivemouse.js
+++ src/controls/Inactivemouse.js
@@ -1,29 +1,29 @@
 function debounce(delay, fn) {
-    let timeoutId = null
-    return ((...args) => {
-        if (timeoutId) {
-            clearTimeout(timeoutId)
-        }
-        timeoutId = setTimeout(() => {
-            timeoutId = null
-            fn(...args)
-        }, delay)
-    })
+  let timeoutId = null
+  return (...args) => {
+    if (timeoutId) {
+      clearTimeout(timeoutId)
+    }
+    timeoutId = setTimeout(() => {
+      timeoutId = null
+      fn(...args)
+    }, delay)
+  }
 }
 
-export function InactiveMouse({INACTIVITY_DELAY = 3000}) {
-    let isIdle = true
-    const hideMouse = debounce(INACTIVITY_DELAY, () => {
-        isIdle = true
-        document.documentElement.style = 'cursor: none'
-    })
-    
-    window.addEventListener('mousemove', () => {
-        if (isIdle) {
-            isIdle = false
-            document.documentElement.style = ''
-        }
-        hideMouse()
-    })
-    return null
+export function InactiveMouse({ INACTIVITY_DELAY = 3000 }) {
+  let isIdle = true
+  const hideMouse = debounce(INACTIVITY_DELAY, () => {
+    isIdle = true
+    document.documentElement.style = 'cursor: none'
+  })
+
+  window.addEventListener('mousemove', () => {
+    if (isIdle) {
+      isIdle = false
+      document.documentElement.style = ''
+    }
+    hideMouse()
+  })
+  return null
 }
