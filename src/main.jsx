ac63dbb6711b565b190baaa493a23dee95b23fcb
Paul Henschel <drcmda@gmail.com>
2021-06-07 19:38:04 +0200

    import App from main

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import ReactDOM from 'react-dom'
 import './styles.css'
-import { App } from './app.jsx'
+import { App } from './App'
 
 //ReactDOM.createRoot(document.getElementById('root')).render(<App />);
 ReactDOM.render(<App />, document.getElementById('root'))
