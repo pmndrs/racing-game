d16d2bc0f4196c969205ce1155c5d0029d14a677
Paul Henschel <drcmda@gmail.com>
2021-06-09 12:56:10 +0200

    prettier

--- src/models/vehicle/Wheel.jsx
+++ src/models/vehicle/Wheel.jsx
@@ -6,7 +6,7 @@ import { useStore } from '../../store'
 useGLTF.preload('/models/wheel-draco.glb')
 
 const Wheel = forwardRef(({ leftSide, ...props }, ref) => {
-  const { radius } = useStore((state) => state.vehicleConfig)  
+  const { radius } = useStore((state) => state.vehicleConfig)
   const { nodes, materials } = useGLTF('/models/wheel-draco.glb')
   const scale = radius / 0.34
   useCylinder(() => ({ mass: 50, type: 'Kinematic', material: 'wheel', collisionFilterGroup: 0, args: [radius, radius, 0.5, 16], ...props }), ref)
