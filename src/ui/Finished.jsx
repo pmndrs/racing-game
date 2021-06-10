9a5a2de450454931e4752aa33e5694b8a3f0939e
njm222 <nathan.john.menezes@gmail.com>
2021-06-10 20:36:46 +0530

    extracted supabase client

--- src/ui/Finished.jsx
+++ src/ui/Finished.jsx
@@ -1,14 +1,13 @@
 import { useStore, mutation } from '../store'
-import { createClient } from '@supabase/supabase-js'
+import { supabase } from '../supabase'
 import { useState } from 'react'
-
-export const supabase = createClient(import.meta.env['VITE_SUPABASE_URL'], import.meta.env['VITE_SUPABASE_ANON_KEY'])
 
 const Finished = () => {
   const LOCAL_STORAGE_KEY = 'racing-pmndrs-name'
-  const [name, setName] = useState('')
   const finished = useStore((state) => state.finished)
   const readableTime = (finished / 1000).toFixed(2)
+
+  const [name, setName] = useState('')
   const [savedName, setSavedName] = useState(window.localStorage.getItem(LOCAL_STORAGE_KEY))
   const [leaderBoard, setLeaderBoard] = useState(null)
   const [position, setPosition] = useState(null)
@@ -31,39 +30,39 @@ const Finished = () => {
     setSavedName(name)
   }
 
-  if (leaderBoard) {
-    return (
-      <div className="finished">
-        <h1> You are number #{position}</h1>
-        <ul className="leaderboard">
-          {leaderBoard.map((score) => (
-            <li key={score.id}>
-              <b>{(score.time / 1000).toFixed(2)}</b>by {score.name}
-            </li>
-          ))}
-        </ul>
-        <Restart />
-      </div>
-    )
-  }
-
   return (
     <div className="finished">
-      <h1>Good job! Your time was {readableTime}</h1>
-      {!savedName ? (
-        <form className="name-form" onSubmit={setUsername}>
-          <h2>You belong in our leaderboards! </h2>
-          <label htmlFor="name">What is your name?</label>
-          <div>
-            <input required maxLength={10} minLength={2} id="name" name="name" value={name} onChange={(e) => setName(e.target.value)} />
-            <button className="popup-item-key">Add me</button>
-          </div>
-        </form>
+      {leaderBoard ? (
+        <>
+          <h1> You are number #{position}</h1>
+          <ul className="leaderboard">
+            {leaderBoard.map((score) => (
+              <li key={score.id}>
+                <b>{(score.time / 1000).toFixed(2)}</b>by {score.name}
+              </li>
+            ))}
+          </ul>
+        </>
       ) : (
-        <button className="popup-item-key add-me" onClick={sendTime}>
-          Add me to the leaderboard
-        </button>
+        <>
+          <h1>Good job! Your time was {readableTime}</h1>
+          {!savedName ? (
+            <form className="name-form" onSubmit={setUsername}>
+              <h2>You belong in our leaderboards! </h2>
+              <label htmlFor="name">What is your name?</label>
+              <div>
+                <input required maxLength={10} minLength={2} id="name" name="name" value={name} onChange={(e) => setName(e.target.value)} />
+                <button className="popup-item-key">Add me</button>
+              </div>
+            </form>
+          ) : (
+            <button className="popup-item-key add-me" onClick={sendTime}>
+              Add me to the leaderboard
+            </button>
+          )}
+        </>
       )}
+
       <Restart />
     </div>
   )
