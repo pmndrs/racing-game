f3264cfee60c493ed08013f1c9ae13014d8ad629
Bjorn Stromberg <bjorn@bjornstar.com>
2021-06-10 20:49:22 +0900

    [main.jsx] Remove commented out code

--- src/main.jsx
+++ src/main.jsx
@@ -3,5 +3,4 @@ import 'inter-ui'
 import './styles.css'
 import { App } from './App'
 
-//ReactDOM.createRoot(document.getElementById('root')).render(<App />);
 ReactDOM.render(<App />, document.getElementById('root'))
