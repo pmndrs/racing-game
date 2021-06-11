d82635805b0d0da496c8f18dddff9e1c21773ba2
Sara Vieira <hey@iamsaravieira.com>
2021-06-11 16:23:11 +0200

    fix app breaking if no env

--- src/utils/initSupabase.js
+++ src/utils/initSupabase.js
@@ -1,3 +1,11 @@
 import { createClient } from '@supabase/supabase-js'
+import { noop } from 'lodash'
 
-export const supabase = createClient(import.meta.env['VITE_SUPABASE_URL'], import.meta.env['VITE_SUPABASE_ANON_KEY'])
+export const supabase = () => {
+  const url = import.meta.env['VITE_SUPABASE_URL']
+  const key = import.meta.env['VITE_SUPABASE_ANON_KEY']
+
+  if (!key || !url) return noop
+
+  return createClient(url, key)
+}
