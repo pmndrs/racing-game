e2125068dcbaf81d9316e344273ad9dd4f929845
Sara Vieira <hey@iamsaravieira.com>
2021-06-11 16:30:26 +0200

    add log

--- src/utils/initSupabase.js
+++ src/utils/initSupabase.js
@@ -5,7 +5,10 @@ export const supabase = () => {
   const url = import.meta.env['VITE_SUPABASE_URL']
   const key = import.meta.env['VITE_SUPABASE_ANON_KEY']
 
-  if (!key || !url) return noop
+  if (!key || !url) {
+    console.error('Missing Environment keys. Please join our discord to have access')
+    return noop
+  }
 
   return createClient(url, key)
 }
