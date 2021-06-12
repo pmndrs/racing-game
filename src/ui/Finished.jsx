d87bdbb773b34db7e83649dc209723314413661f
Bjorn Stromberg <bjorn@bjornstar.com>
2021-06-12 12:27:53 +0900

    Cleanup leaderboard
    - [Store] Created a `reset` method that handles everything needed to reset the game
    - [Store] `reset` is no longer a property on `control`
    - [utils] Moved everything from `utils` into a single data file.
    - [data] Added a default config that can be overridden by environment variables
    - [data] Don't create a new client on every request
    - [data] Changed method names to reflect the name of the database: `scores`
    - [Finished] Tweaked wording: "You belong on our leaderboard!"
    - [Leaderboard] Fixed bug where clicking on the button would delete the game state
    - [Leaderboard] Fetch scores when opening the leaderboard and no more than once every 3 seconds
    - [Vehicle] renamed `ctrl` to `controls`
    - [Vehicle] imported only the used methods from `three`
    - [Vehicle] use maxSpeed instead of vehicleConfig.maxSpeed
    - [Store] Moved `angularVelocity`, `position`, & `rotation` back to the store
    - [Store] Don't export wheelInfos

--- src/ui/Finished.jsx
+++ src/ui/Finished.jsx
@@ -1,7 +1,7 @@
-import { useStore, mutation } from '../store'
 import { useState } from 'react'
-import { getLeaderBoardData, insertTime } from '../utils/data/leaderboard'
-import { Item } from './LeaderBoard'
+import { reset, useStore } from '../store'
+import { getScores, insertScore } from '../data'
+import { Score } from './LeaderBoard'
 
 export const Finished = () => {
   const LOCAL_STORAGE_KEY = 'racing-pmndrs-name'
@@ -9,14 +9,14 @@ export const Finished = () => {
   const finished = useStore((state) => state.finished)
   const readableTime = (finished / 1000).toFixed(2)
   const [savedName, setSavedName] = useState(window.localStorage.getItem(LOCAL_STORAGE_KEY))
-  const [leaderBoard, setLeaderBoard] = useState(null)
+  const [scores, setScores] = useState(null)
   const [position, setPosition] = useState(null)
 
-  const sendTime = async () => {
-    const newTime = await insertTime({ time: finished, name: savedName })
-    const leaderboardData = await getLeaderBoardData()
-    setLeaderBoard(leaderboardData)
-    setPosition(leaderboardData.findIndex((l) => l.id === newTime[0].id) + 1)
+  const addMe = async () => {
+    const [{ id }] = await insertScore({ time: finished, name: savedName })
+    const scores = await getScores()
+    setScores(scores)
+    setPosition(scores.findIndex((score) => score.id === id) + 1)
   }
 
   const setUsername = (e) => {
@@ -25,13 +25,13 @@ export const Finished = () => {
     setSavedName(name)
   }
 
-  if (leaderBoard) {
+  if (scores) {
     return (
       <div className="finished">
         <h1> You are number #{position}</h1>
         <ul className="leaderboard">
-          {leaderBoard.map((score) => (
-            <Item {...score} key={score.id} />
+          {scores.map((score, key) => (
+            <Score {...score} key={key} />
           ))}
         </ul>
         <Restart />
@@ -44,7 +44,7 @@ export const Finished = () => {
       <h1>Good job! Your time was {readableTime}</h1>
       {!savedName ? (
         <form className="name-form" onSubmit={setUsername}>
-          <h2>You belong in our leaderboards! </h2>
+          <h2>You belong on our leaderboard! </h2>
           <label htmlFor="name">What is your name?</label>
           <div>
             <input required maxLength={10} minLength={2} id="name" name="name" value={name} onChange={(e) => setName(e.target.value)} />
@@ -52,7 +52,7 @@ export const Finished = () => {
           </div>
         </form>
       ) : (
-        <button className="popup-item-key add-me" onClick={sendTime}>
+        <button className="popup-item-key add-me" onClick={addMe}>
           Add me to the leaderboard
         </button>
       )}
@@ -63,10 +63,8 @@ export const Finished = () => {
 
 const Restart = () => {
   const set = useStore((state) => state.set)
-  const cleanState = () =>
-    set((state) => ((mutation.start = 0), (mutation.finish = 0), { ...state, finished: false, controls: { ...state.controls, reset: true } }))
   return (
-    <button className="restart" onClick={cleanState}>
+    <button className="restart" onClick={() => reset(set)}>
       <div>Restart</div>
     </button>
   )
