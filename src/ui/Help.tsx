23c4a77c6e7e43944679870c1e3f45bf35e06e27
Gusted <williamzijl7@hotmail.com>
2021-06-13 11:08:56 +0200

    Convert help.jsx

--- src/ui/Help.tsx
+++ src/ui/Help.tsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import { useStore } from '../store'
 
 const controlOptions = [
@@ -37,7 +38,7 @@ export function Help() {
   )
 }
 
-export function Keys(props) {
+export function Keys(props: React.HTMLAttributes<HTMLDivElement>) {
   return (
     <div {...props}>
       {controlOptions.map(({ keys, action }) => (
