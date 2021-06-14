d1c822f0b7e417991ea215aa8926ad86d84c5e60
Bjorn Stromberg <bjorn@bjornstar.com>
2021-06-14 10:49:20 +0900

    Differentiate between saved and unsaved scores

--- src/ui/Finished.tsx
+++ src/ui/Finished.tsx
@@ -1,50 +1,51 @@
 import { useState } from 'react'
 import { reset, useStore } from '../store'
 import { getScores, insertScore } from '../data'
-import type { IScore } from '../data'
-import { Score } from './LeaderBoard'
+import { readableTime, Scores } from './LeaderBoard'
 import { Auth } from './Auth'
+import type { SavedScore } from '../data'
 
-export const Finished = () => {
-  const [finished, session] = useStore((state) => [state.finished, state.session])
-  const readableTime = (finished / 1000).toFixed(2)
+const Restart = (): JSX.Element => {
+  const set = useStore((state) => state.set)
+  return (
+    <button className="restart" onClick={() => reset(set)}>
+      <div>Restart</div>
+    </button>
+  )
+}
 
-  const [scores, setScores] = useState<IScore[]>(null!)
-  const [position, setPosition] = useState<number>(null!)
+export const Finished = (): JSX.Element => {
+  const [time, session] = useStore((state) => [state.finished, state.session])
+  const [scores, setScores] = useState<SavedScore[]>([])
+  const [position, setPosition] = useState<number>(0)
+
+  const user = session?.user?.user_metadata
+  const name: string = user?.full_name
+  const thumbnail: string = user?.avatar_url
 
   const sendScore = async () => {
-    const user = session?.user?.user_metadata
-    const [{ id }] = (await insertScore({ time: finished, name: user?.full_name, thumbnail: user?.avatar_url })) || [{}]
-    if (!id) {
-      return
-    }
+    const [{ id }] = await insertScore({ name, thumbnail, time })
     const scores = await getScores()
-    if (!scores) {
-      return
-    }
     setScores(scores)
-    setPosition(scores.findIndex((score) => score.id === id) + 1)
+    const index = scores.findIndex((score) => score.id === id)
+    setPosition(index && index + 1)
   }
 
   return (
     <div className="finished">
       {scores ? (
         <>
-          <h1> You are number #{position}</h1>
-          <ul className="leaderboard">
-            {scores.map((score, key) => (
-              <Score {...score} standing={key} key={key} />
-            ))}
-          </ul>
+          {position && <h1>You are number #{position}</h1>}
+          <Scores className="leaderboard" scores={scores} />
         </>
       ) : (
         <>
-          <h1>Good job! Your time was {readableTime}</h1>
+          <h1>Good job! Your time was {readableTime(time)}</h1>
           {session?.user?.aud !== 'authenticated' ? (
             <Auth />
           ) : (
             <>
-              <h2>You belong on our leaderboard {session.user.user_metadata.full_name}! </h2>
+              <h2>You belong on our leaderboard {name}! </h2>
               <button onClick={sendScore} style={{ margin: '0 auto', width: 'auto' }} className="popup-item-key">
                 Add my score
               </button>
@@ -54,14 +55,5 @@ export const Finished = () => {
       )}
       <Restart />
     </div>
-  )
-}
-
-const Restart = () => {
-  const set = useStore((state) => state.set)
-  return (
-    <button className="restart" onClick={() => reset(set)}>
-      <div>Restart</div>
-    </button>
   )
 }
--- src/ui/LeaderBoard.tsx
+++ src/ui/LeaderBoard.tsx
@@ -1,58 +1,72 @@
 import { useEffect, useState } from 'react'
-import type { IScore } from '../data'
 import { getScores } from '../data'
 import { useStore } from '../store'
+import type { SavedScore } from '../data'
 
-export function LeaderBoard() {
-  const [scores, setScores] = useState<IScore[]>([])
+export function LeaderBoard(): JSX.Element {
+  const [scores, setScores] = useState<SavedScore[]>([])
   const [last, setLast] = useState(0)
-  const [set, leaderboard] = useStore((state) => [state.set, state.leaderboard])
+  const [leaderboard, set] = useStore((state) => [state.leaderboard, state.set])
+  const close = () => set((state) => ({ ...state, leaderboard: false }))
+
   useEffect(() => {
     if (!leaderboard || Date.now() - last < 3000) return
-    getScores(10).then(setScores as (value: IScore[] | null) => void | PromiseLike<void>)
+    getScores(10).then(setScores)
     setLast(Date.now())
   }, [leaderboard])
-  const close = () => set((state) => ({ ...state, leaderboard: false }))
+
   return (
     <div className="controls">
       <div className={`popup ${leaderboard ? 'open' : ''}`}>
         <button className="popup-close" onClick={close}>
           L
         </button>
-        <ul className="popup-content leaderboard-bottom">
-          {scores.map((score, key) => (
-            <Score {...score} standing={key} key={key} />
-          ))}
-        </ul>
+        <Scores className="popup-content leaderboard-bottom" scores={scores} />
       </div>
     </div>
   )
 }
 
-const standingToImage: { [number: string]: string } = {
+const standingToImage: Record<1 | 2 | 3, string> = {
   1: 'images/gold.png',
   2: 'images/silver.png',
   3: 'images/bronze.png',
 }
 
-export const Score = ({ name, time, standing }: { name: string; time: number; standing: number }) => {
-  const isTop = standing < 3
-
-  return (
-    <li className="popup-item">
-      <span className="leaderboard-name">
-        {isTop ? (
-          <>
-            <img src={standingToImage[String(standing + 1)]} width={13} height={13} />
-            {name}
-          </>
-        ) : (
-          `${standing + 1} ${name}`
-        )}
-      </span>
-      <span className="popup-item-key">
-        <span>{(time / 1000).toFixed(2)}</span>
-      </span>
-    </li>
-  )
+type ScoreProps = {
+  name: string
+  standing: number
+  time: number
 }
+
+export const readableTime = (time: number): string => (time / 1000).toFixed(2)
+
+interface ScoresProps extends React.HTMLAttributes<HTMLUListElement> {
+  scores: SavedScore[]
+}
+
+const Score = ({ name, standing, time }: ScoreProps): JSX.Element => (
+  <li className="popup-item">
+    <span className="leaderboard-name">
+      {standing === 1 || standing === 2 || standing === 3 ? (
+        <>
+          <img src={standingToImage[standing]} width={13} height={13} />
+          {name}
+        </>
+      ) : (
+        `${standing} ${name}`
+      )}
+    </span>
+    <span className="popup-item-key">
+      <span>{readableTime(time)}</span>
+    </span>
+  </li>
+)
+
+export const Scores = ({ scores, ...props }: ScoresProps): JSX.Element => (
+  <ul {...props}>
+    {scores.map((score, index) => (
+      <Score {...score} standing={index + 1} key={index} />
+    ))}
+  </ul>
+)
