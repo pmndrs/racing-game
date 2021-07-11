6d3749d9f4a499aa7865def6ad03be2c1992c82a
Bjorn Stromberg <bjorn@bjornstar.com>
2021-07-11 11:11:28 +0900

    useToggle for Debug

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import { Canvas } from '@react-three/fiber'
 import { Physics, Debug } from '@react-three/cannon'
 import { Sky, Environment, PerspectiveCamera, OrthographicCamera, OrbitControls, Stats } from '@react-three/drei'
 
-import type { ReactNode } from 'react'
 import type { DirectionalLight } from 'three'
 
 import { HideMouse, Keyboard } from './controls'
@@ -16,23 +15,13 @@ import { useToggle } from './useToggle'
 const layers = new Layers()
 layers.enable(levelLayer)
 
-function DebugScene({ children }: { children: ReactNode }) {
-  const debug = useStore((state) => state.debug)
-  return debug ? (
-    <Debug scale={1.0001} color="white">
-      {children}
-    </Debug>
-  ) : (
-    <>{children}</>
-  )
-}
-
 export function App() {
   const [light, setLight] = useState<DirectionalLight>()
   const [actions, camera, dpr, editor, shadows] = useStore((s) => [s.actions, s.camera, s.dpr, s.editor, s.shadows])
   const { onCheckpoint, onFinish, onStart } = actions
 
   const ToggledCheckpoint = useToggle(Checkpoint, 'checkpoint')
+  const ToggledDebug = useToggle(Debug, 'debug')
   const ToggledEditor = useToggle(Editor, 'editor')
   const ToggledFinished = useToggle(Finished, 'finished')
   const ToggledMap = useToggle(Minimap, 'map')
@@ -60,7 +49,7 @@ export function App() {
         />
         <PerspectiveCamera makeDefault={editor} fov={75} position={[0, 20, 20]} />
         <Physics broadphase="SAP" defaultContactMaterial={{ contactEquationRelaxation: 4, friction: 1e-3 }} allowSleep>
-          <DebugScene>
+          <ToggledDebug scale={1.0001} color="white">
             <Vehicle angularVelocity={[...angularVelocity]} position={[...position]} rotation={[...rotation]}>
               {light && <primitive object={light.target} />}
               <PerspectiveCamera makeDefault={!editor && camera !== 'BIRD_EYE'} fov={75} rotation={[0, Math.PI, 0]} position={[0, 10, -20]} />
@@ -72,7 +61,7 @@ export function App() {
             <Goal args={[0.001, 10, 18]} onCollideBegin={onStart} rotation={[0, 0.55, 0]} position={[-27, 1, 180]} />
             <Goal args={[0.001, 10, 18]} onCollideBegin={onFinish} rotation={[0, -1.2, 0]} position={[-104, 1, -189]} />
             <Goal args={[0.001, 10, 18]} onCollideBegin={onCheckpoint} rotation={[0, -0.5, 0]} position={[-50, 1, -5]} />
-          </DebugScene>
+          </ToggledDebug>
         </Physics>
         <Track />
         <Environment files="textures/dikhololo_night_1k.hdr" />
--- src/useToggle.tsx
+++ src/useToggle.tsx
@@ -1,11 +1,11 @@
 import { useStore } from './store'
 
-import type { ComponentType } from 'react'
+import type { ComponentType, PropsWithChildren } from 'react'
 import type { IState } from './store'
 
 export const useToggle =
   <P extends {}>(ToggledComponent: ComponentType<P>, toggle: keyof IState) =>
-  (props: P) => {
+  (props: PropsWithChildren<P>) => {
     const value = useStore((state) => state[toggle])
-    return value ? <ToggledComponent {...props} /> : null
+    return value ? <ToggledComponent {...props} /> : props.children ? <>{props.children}</> : null
   }
