d4d9896ffd41e026b319cd3e53ab7043d3e0fd09
a.muhin <a.muhin@a.muhin>
2022-03-27 13:46:20 +0400

    saveable keybindings

--- src/store.ts
+++ src/store.ts
@@ -5,7 +5,8 @@ import type { RefObject } from 'react'
 import type { PublicApi, WheelInfoOptions } from '@react-three/cannon'
 import type { Session } from '@supabase/supabase-js'
 import type { Group, Object3D } from 'three'
-import type { GetState, SetState, StateSelector } from 'zustand'
+import type { StateSelector, GetState, SetState, StoreApi, Mutate } from 'zustand'
+import { subscribeWithSelector } from 'zustand/middleware'
 
 export const angularVelocity = [0, 0.5, 0] as const
 export const cameras = ['DEFAULT', 'FIRST_PERSON', 'BIRD_EYE'] as const
@@ -104,7 +105,8 @@ export interface KeyConfig extends KeyMap {
 }
 
 export interface IState extends BaseState {
-  actions: Record<ActionNames, (() => void) | ((action: string, newKey: Key) => void) | ((action: string, name: string) => void)>
+  // actions: Record<ActionNames, (() => void) | ((action: string, newKey: Key) => void) | ((action: string, name: string) => void)>
+  actions: Record<ActionNames, any>
   api: PublicApi | null
   bestCheckpoint: number
   camera: Camera
@@ -140,185 +142,228 @@ function checkKeybindings(keysList: KeyConfig[]): number[] {
   }, [])
 }
 
-const useStoreImpl = create<IState>((set: SetState<IState>, get: GetState<IState>) => {
-  const actions = {
-    onCheckpoint: () => {
-      const { start } = get()
-      if (start) {
-        const checkpoint = Date.now() - start
-        set({ checkpoint })
-      }
-    },
-    onFinish: () => {
-      const { finished, start } = get()
-      if (start && !finished) {
-        set({ finished: Math.max(Date.now() - start, 0) })
-      }
-    },
-    onStart: () => {
-      set({ finished: 0, start: Date.now() })
-    },
-    reset: () => {
-      mutation.boost = maxBoost
+function serializeKeys(keysList: KeyConfig[]): Record<string, Key[]> {
+  return keysList.reduce((akk, { action, keys }) => ({ ...akk, [action]: keys }), {})
+}
 
-      set((state) => {
-        state.api?.angularVelocity.set(...angularVelocity)
-        state.api?.position.set(...position)
-        state.api?.rotation.set(...rotation)
-        state.api?.velocity.set(0, 0, 0)
+const localStorageKeyboardBindingsString = window.localStorage.getItem('pmndrs-racing-game-keybindings')
 
-        return { ...state, finished: 0, start: 0 }
-      })
-    },
-    addKeyBinding: (action: string, newKey: Key) => {
-      set((state) => {
-        const index = state.keyboardBindings.findIndex(({ action: stateAction }) => action === stateAction)
+const setLocalStorageKeyboardBindingsString = (newValue: string) => {
+  window.localStorage.setItem('pmndrs-racing-game-keybindings', newValue)
+}
 
-        if (index === -1) {
-          return state
+let localStorageKeyboardBindings: Record<string, Key[]> = {}
+
+if (localStorageKeyboardBindingsString !== null) {
+  try {
+    localStorageKeyboardBindings = JSON.parse(localStorageKeyboardBindingsString)
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+const useStoreImpl = create<IState, SetState<IState>, GetState<IState>, Mutate<StoreApi<IState>, [['zustand/subscribeWithSelector', never]]>>(
+  subscribeWithSelector((set, get) => {
+    const actions = {
+      onCheckpoint: () => {
+        const { start } = get()
+        if (start) {
+          const checkpoint = Date.now() - start
+          set({ checkpoint })
         }
+      },
+      onFinish: () => {
+        const { finished, start } = get()
+        if (start && !finished) {
+          set({ finished: Math.max(Date.now() - start, 0) })
+        }
+      },
+      onStart: () => {
+        set({ finished: 0, start: Date.now() })
+      },
+      reset: () => {
+        mutation.boost = maxBoost
 
-        const keyboardBindingsCopy = deduplicateKeys(newKey, state.keyboardBindings)
+        set((state) => {
+          state.api?.angularVelocity.set(...angularVelocity)
+          state.api?.position.set(...position)
+          state.api?.rotation.set(...rotation)
+          state.api?.velocity.set(0, 0, 0)
 
-        if (index !== -1) {
+          return { ...state, finished: 0, start: 0 }
+        })
+      },
+      addKeyBinding: (action: string, newKey: Key) => {
+        set((state) => {
+          const index = state.keyboardBindings.findIndex(({ action: stateAction }) => action === stateAction)
+
+          if (index === -1) {
+            return state
+          }
+
+          const keyboardBindingsCopy = deduplicateKeys(newKey, state.keyboardBindings)
+
           keyboardBindingsCopy[index].keys.push(newKey)
-        }
 
-        const keyBindingsWithError = checkKeybindings(keyboardBindingsCopy)
+          const keyBindingsWithError = checkKeybindings(keyboardBindingsCopy)
 
-        return { ...state, keyboardBindings: keyboardBindingsCopy, keyBindingsWithError }
-      })
-    },
-    removeKeyBinding: (action: string, name: string) => {
-      set((state) => {
-        const index = state.keyboardBindings.findIndex(({ action: stateAction }) => action === stateAction)
+          return { ...state, keyboardBindings: keyboardBindingsCopy, keyBindingsWithError }
+        })
+      },
+      removeKeyBinding: (action: string, name: string) => {
+        set((state) => {
+          const index = state.keyboardBindings.findIndex(({ action: stateAction }) => action === stateAction)
 
-        if (index === -1) {
-          return state
-        }
+          if (index === -1) {
+            return state
+          }
 
-        const keyIndex = state.keyboardBindings[index].keys.findIndex(({ name: stateName }) => name === stateName)
+          const keyIndex = state.keyboardBindings[index].keys.findIndex(({ name: stateName }) => name === stateName)
 
-        const keyboardBindingsCopy = [...state.keyboardBindings]
+          const keyboardBindingsCopy = [...state.keyboardBindings]
 
-        if (index !== -1) {
           keyboardBindingsCopy[index].keys.splice(keyIndex, 1)
-        }
 
-        const keyBindingsWithError = checkKeybindings(keyboardBindingsCopy)
+          const keyBindingsWithError = checkKeybindings(keyboardBindingsCopy)
 
-        return { ...state, keyboardBindings: keyboardBindingsCopy, keyBindingsWithError }
-      })
-    },
-  }
+          return { ...state, keyboardBindings: keyboardBindingsCopy, keyBindingsWithError }
+        })
+      },
+    }
 
-  const defaultKeyboardBindings: KeyConfig[] = [
-    {
-      action: 'Forward',
-      keys: [
-        { name: '↑', values: ['ArrowUp'] },
-        { name: 'W', values: ['KeyW'] },
-        { name: 'Z', values: ['KeyZ'] },
-      ],
+    const defaultKeyboardBindings: KeyConfig[] = [
+      {
+        action: 'Forward',
+        keys: localStorageKeyboardBindings.Forward || [
+          { name: '↑', values: ['ArrowUp'] },
+          { name: 'W', values: ['KeyW'] },
+          { name: 'Z', values: ['KeyZ'] },
+        ],
 
-      fn: (forward) => set((state) => ({ controls: { ...state.controls, forward } })),
-    },
-    {
-      action: 'Backward',
-      keys: [
-        { name: '↓', values: ['ArrowDown'] },
-        { name: 'S', values: ['KeyS'] },
-      ],
-      fn: (backward) => set((state) => ({ controls: { ...state.controls, backward } })),
-    },
-    {
-      action: 'Left',
-      keys: [
-        { name: '←', values: ['ArrowLeft'] },
-        { name: 'A', values: ['KeyA'] },
-        { name: 'Q', values: ['KeyQ'] },
-      ],
-      fn: (left) => set((state) => ({ controls: { ...state.controls, left } })),
-    },
-    {
-      action: 'Right',
-      keys: [
-        { name: '→', values: ['ArrowRight'] },
-        { name: 'D', values: ['KeyD'] },
-      ],
-      fn: (right) => set((state) => ({ controls: { ...state.controls, right } })),
-    },
-    { action: 'Drift', keys: [{ name: 'Space ␣', values: ['Space'] }], fn: (brake) => set((state) => ({ controls: { ...state.controls, brake } })) },
-    { action: 'Honk', keys: [{ name: 'H', values: ['KeyH'] }], fn: (honk) => set((state) => ({ controls: { ...state.controls, honk } })) },
-    {
-      action: 'Turbo Boost',
-      keys: [{ name: 'Shift ⇧', values: ['ShiftLeft', 'ShiftRight'] }],
-      fn: (boost) => set((state) => ({ controls: { ...state.controls, boost } })),
-    },
-    { action: 'Reset', keys: [{ name: 'R', values: ['KeyR'] }], fn: actions.reset, up: false },
-    { action: 'Editor', keys: [{ name: '.', values: ['Period'] }], fn: () => set((state) => ({ editor: !state.editor })), up: false },
-    {
-      action: 'Help',
-      keys: [{ name: 'I', values: ['KeyI'] }],
-      fn: () => set((state) => ({ help: !state.help, leaderboard: false, pickcolor: false })),
-      up: false,
-    },
-    {
-      action: 'Leaderboards',
-      keys: [{ name: 'L', values: ['KeyL'] }],
-      fn: () => set((state) => ({ help: false, leaderboard: !state.leaderboard, pickcolor: false })),
-      up: false,
-    },
-    { action: 'Map', keys: [{ name: 'M', values: ['KeyM'] }], fn: () => set((state) => ({ map: !state.map })), up: false },
-    {
-      action: 'Pick Car Color',
-      keys: [{ name: 'P', values: ['KeyP'] }],
-      fn: () => set((state) => ({ help: false, pickcolor: !state.pickcolor, leaderboard: false })),
-      up: false,
-    },
-    { action: 'Toggle Mute', keys: [{ name: 'U', values: ['KeyU'] }], fn: () => set((state) => ({ sound: !state.sound })), up: false },
-    {
-      action: 'Toggle Camera',
-      keys: [{ name: 'C', values: ['KeyC'] }],
-      fn: () => set((state) => ({ camera: cameras[(cameras.indexOf(state.camera) + 1) % cameras.length] })),
-      up: false,
-    },
-  ]
+        fn: (forward) => set((state) => ({ controls: { ...state.controls, forward } })),
+      },
+      {
+        action: 'Backward',
+        keys: localStorageKeyboardBindings.Backward || [
+          { name: '↓', values: ['ArrowDown'] },
+          { name: 'S', values: ['KeyS'] },
+        ],
+        fn: (backward) => set((state) => ({ controls: { ...state.controls, backward } })),
+      },
+      {
+        action: 'Left',
+        keys: localStorageKeyboardBindings.Left || [
+          { name: '←', values: ['ArrowLeft'] },
+          { name: 'A', values: ['KeyA'] },
+          { name: 'Q', values: ['KeyQ'] },
+        ],
+        fn: (left) => set((state) => ({ controls: { ...state.controls, left } })),
+      },
+      {
+        action: 'Right',
+        keys: localStorageKeyboardBindings.Right || [
+          { name: '→', values: ['ArrowRight'] },
+          { name: 'D', values: ['KeyD'] },
+        ],
+        fn: (right) => set((state) => ({ controls: { ...state.controls, right } })),
+      },
+      {
+        action: 'Drift',
+        keys: localStorageKeyboardBindings.Drift || [{ name: 'Space ␣', values: ['Space'] }],
+        fn: (brake) => set((state) => ({ controls: { ...state.controls, brake } })),
+      },
+      {
+        action: 'Honk',
+        keys: localStorageKeyboardBindings.Honk || [{ name: 'H', values: ['KeyH'] }],
+        fn: (honk) => set((state) => ({ controls: { ...state.controls, honk } })),
+      },
+      {
+        action: 'Turbo Boost',
+        keys: localStorageKeyboardBindings['Turbo Boost'] || [{ name: 'Shift ⇧', values: ['ShiftLeft', 'ShiftRight'] }],
+        fn: (boost) => set((state) => ({ controls: { ...state.controls, boost } })),
+      },
+      { action: 'Reset', keys: localStorageKeyboardBindings.Reset || [{ name: 'R', values: ['KeyR'] }], fn: actions.reset, up: false },
+      {
+        action: 'Editor',
+        keys: localStorageKeyboardBindings.Editor || [{ name: '.', values: ['Period'] }],
+        fn: () => set((state) => ({ editor: !state.editor })),
+        up: false,
+      },
+      {
+        action: 'Help',
+        keys: localStorageKeyboardBindings.Help || [{ name: 'I', values: ['KeyI'] }],
+        fn: () => set((state) => ({ help: !state.help, leaderboard: false, pickcolor: false })),
+        up: false,
+      },
+      {
+        action: 'Leaderboards',
+        keys: localStorageKeyboardBindings.Leaderboards || [{ name: 'L', values: ['KeyL'] }],
+        fn: () => set((state) => ({ help: false, leaderboard: !state.leaderboard, pickcolor: false })),
+        up: false,
+      },
+      {
+        action: 'Map',
+        keys: localStorageKeyboardBindings.Map || [{ name: 'M', values: ['KeyM'] }],
+        fn: () => set((state) => ({ map: !state.map })),
+        up: false,
+      },
+      {
+        action: 'Pick Car Color',
+        keys: localStorageKeyboardBindings['Pick Car Color'] || [{ name: 'P', values: ['KeyP'] }],
+        fn: () => set((state) => ({ help: false, pickcolor: !state.pickcolor, leaderboard: false })),
+        up: false,
+      },
+      {
+        action: 'Toggle Mute',
+        keys: localStorageKeyboardBindings['Toggle Mute'] || [{ name: 'U', values: ['KeyU'] }],
+        fn: () => set((state) => ({ sound: !state.sound })),
+        up: false,
+      },
+      {
+        action: 'Toggle Camera',
+        keys: localStorageKeyboardBindings['Toggle Camera'] || [{ name: 'C', values: ['KeyC'] }],
+        fn: () => set((state) => ({ camera: cameras[(cameras.indexOf(state.camera) + 1) % cameras.length] })),
+        up: false,
+      },
+    ]
 
-  return {
-    actions,
-    api: null,
-    bestCheckpoint: 0,
-    camera: cameras[0],
-    chassisBody: createRef<Object3D>(),
-    checkpoint: 0,
-    color: '#FFFF00',
-    controls,
-    keyboardBindings: defaultKeyboardBindings,
-    keyBindingsWithError: [],
-    debug,
-    dpr,
-    editor: false,
-    finished: 0,
-    get,
-    keyInput: null,
-    help: false,
-    leaderboard: false,
-    level: createRef<Group>(),
-    map: true,
-    pickcolor: false,
-    ready: false,
-    session: null,
-    set,
-    shadows,
-    sound: true,
-    start: 0,
-    stats,
-    vehicleConfig,
-    wheelInfo,
-    wheels: [createRef<Object3D>(), createRef<Object3D>(), createRef<Object3D>(), createRef<Object3D>()],
-  }
-})
+    const state: IState = {
+      actions,
+      api: null,
+      bestCheckpoint: 0,
+      camera: cameras[0],
+      chassisBody: createRef<Object3D>(),
+      checkpoint: 0,
+      color: '#FFFF00',
+      controls,
+      keyboardBindings: defaultKeyboardBindings,
+      keyBindingsWithError: [],
+      debug,
+      dpr,
+      editor: false,
+      finished: 0,
+      get,
+      keyInput: null,
+      help: false,
+      leaderboard: false,
+      level: createRef<Group>(),
+      map: true,
+      pickcolor: false,
+      ready: false,
+      session: null,
+      set,
+      shadows,
+      sound: true,
+      start: 0,
+      stats,
+      vehicleConfig,
+      wheelInfo,
+      wheels: [createRef<Object3D>(), createRef<Object3D>(), createRef<Object3D>(), createRef<Object3D>()],
+    }
+
+    return state
+  }),
+)
 
 interface Mutation {
   boost: number
@@ -342,5 +387,12 @@ const useStore = <T>(sel: StateSelector<IState, T>) => useStoreImpl(sel, shallow
 Object.assign(useStore, useStoreImpl)
 
 const { getState, setState } = useStoreImpl
+
+useStoreImpl.subscribe(
+  (state) => state.keyboardBindings,
+  (keyboardBindings) => {
+    setLocalStorageKeyboardBindingsString(JSON.stringify(serializeKeys(keyboardBindings)))
+  },
+)
 
 export { getState, setState, useStore }
