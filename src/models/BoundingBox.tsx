46aead1fe7c2acf11a795a10ee3deef0165166d4
a.muhin <a.muhin@a.muhin>
2022-04-12 18:39:43 +0400

    build fixes

--- src/models/BoundingBox.tsx
+++ src/models/BoundingBox.tsx
@@ -11,7 +11,7 @@ type Props = {
 }
 
 export const BoundingBox = ({ depth, height, position: [x, y, z], width }: Props) => {
-  const { reset: onCollide } = useStore(({ actions: { reset } }) => ({ reset }))
+  const { reset: onCollide } = useStore(({ actions: { reset } }) => ({ reset: reset as () => void }))
 
   const sharedProps = {
     isTrigger: true,
