2d9ad1deb91eb3e8592a8e3428909a7480f43f70
Bjorn Stromberg <bjorn@bjornstar.com>
2022-04-19 10:18:13 +0200

    useRef instead of createRef

--- src/models/Ramp.tsx
+++ src/models/Ramp.tsx
@@ -1,12 +1,11 @@
-import { createRef } from 'react'
+import { useRef } from 'react'
 import { useBox } from '@react-three/cannon'
 
 import type { BoxProps } from '@react-three/cannon'
 import type { Mesh } from 'three'
 
 export function Ramp({ args, ...props }: BoxProps) {
-  const ref = createRef<Mesh>()
-  useBox(() => ({ type: 'Static', args, ...props }), ref, [args, props])
+  const [ref] = useBox(() => ({ type: 'Static', args, ...props }), useRef<Mesh>(null), [args, props])
 
   return (
     <mesh castShadow receiveShadow ref={ref}>
